8356212b45cb49abb0fd5c12eee99deb5ac6201f
Jim Donovan <jimdon326@hotmail.co.uk>
2022-03-31 22:18:38 +0100

    remove looping from pokemon call. Use map and forkJoin for multiple API calls

--- src/app/region/region-pokemon/region-pokemon.component.ts
+++ src/app/region/region-pokemon/region-pokemon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/pokemon.service';
 import { ActivatedRoute } from '@angular/router';
 import { Region } from 'src/app/region/region';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-region-pokemon',
@@ -11,6 +12,9 @@ import { Region } from 'src/app/region/region';
 export class RegionPokemonComponent implements OnInit {
   pokemon: any[] = [];
   region!: Region;
+  pageLimit: number = 10;
+  currentPage: number = 1;
+
   constructor(
     private pokemonService: PokemonService,
     private route: ActivatedRoute
@@ -20,21 +24,22 @@ export class RegionPokemonComponent implements OnInit {
     this.getPokemon();
   }
 
-  getPokemon(): void {
+  getPokemon(startIndex: number = 0): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.pokemonService.getRegionById(id).subscribe((region: Region) => {
       this.region = region;
 
       this.pokemonService
-        .getPokemonList(this.region.startIndex)
+        .getPokemonList(this.region.startIndex, this.pageLimit)
         .subscribe((response: any) => {
-          response.results.forEach((result: any) => {
-            this.pokemonService
-              .getPokemonByUrl(result.url)
-              .subscribe((pokemon) => {
-                this.pokemon.push(pokemon);
-              });
+          let pokemonRequest = response.results.map((result: any) => {
+            return this.pokemonService.getPokemonByUrl(result.url);
+          });
+
+          forkJoin(pokemonRequest).subscribe((pokemon) => {
+            this.pokemon = pokemon;
+            this.sortPokemon();
           });
         });
     });
@@ -43,4 +48,6 @@ export class RegionPokemonComponent implements OnInit {
   sortPokemon(): void {
     this.pokemon.sort((first, second) => first.id - second.id);
   }
+
+  nextPage(): void {}
 }
